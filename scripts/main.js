4d3677dc6e2ae3938967472590fb177552a953d5
bryancurtis <curtispro97@gmail.com>
2021-11-17 13:31:20 -0800

    adds nav and projects section

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+function burgerClick() {
+    const jazz = document.getElementById("burger");
+    console.log("burger clicked");
+}
+
+function nameClick() {
+    window.location.reload(false); 
+}
