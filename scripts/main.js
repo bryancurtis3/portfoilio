8e246652ad6c568a6e0d40d0d79b35232f31f1c4
bryancurtis <curtispro97@gmail.com>
2021-11-21 12:03:52 -0800

    finishes projects sections, adds footer and burger overlay

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,20 @@
 function burgerClick() {
-    const jazz = document.getElementById("burger");
-    console.log("burger clicked");
+    const overlay = document.getElementById("overlay");
+    const body = document.getElementById("body");
+    const burger = document.getElementById("burger");
+    const burgerX = document.getElementById("burger-x");
+    overlay.style.height = "100vh";
+    if (overlay.style.visibility === "visible") {
+        overlay.style.visibility = "hidden";
+        body.style.overflow = "scroll";
+        burger.style.display = "block";
+        burgerX.style.display = "none";
+    } else {
+        overlay.style.visibility = "visible"; 
+        body.style.overflow = "hidden";
+        burger.style.display = "none";
+        burgerX.style.display = "block";
+    }
 }
 
 function nameClick() {
