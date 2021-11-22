67b63ef4077da60d2f90ff2b4d6c10a997cdd00f
bryancurtis <curtispro97@gmail.com>
2021-11-21 16:44:44 -0800

    spent too much time trying to make the overlay blur fade in and out. giving up for now

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,26 @@
 function burgerClick() {
+    console.log("t");
     const overlay = document.getElementById("overlay");
+    const overlayInner = document.getElementById("overlay-inner");
+    const main = document.getElementById("main");
     const body = document.getElementById("body");
     const burger = document.getElementById("burger");
     const burgerX = document.getElementById("burger-x");
     overlay.style.height = "100vh";
-    if (overlay.style.visibility === "visible") {
+    if (overlayInner.style.top === "0px") {
+        console.log("if");
         overlay.style.visibility = "hidden";
+        overlayInner.style.top = "-100vh";
         body.style.overflow = "scroll";
-        burger.style.display = "block";
-        burgerX.style.display = "none";
+        burger.style.transform = "scale(1)";
+        burgerX.style.transform = "scale(0)";
     } else {
-        overlay.style.visibility = "visible"; 
+        console.log("else")
+        overlay.style.visibility = "visible";
+        overlayInner.style.top = "0px";
         body.style.overflow = "hidden";
-        burger.style.display = "none";
-        burgerX.style.display = "block";
+        burger.style.transform = "scale(0)";
+        burgerX.style.transform = "scale(1)";
     }
 }
 
